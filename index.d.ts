ce1cdec3f3e7a4fff65b867e598512f58f4872b3
Félicien François <felicien@alvarum.com>
2026-04-08 15:29:38 +0200

    improves props types

--- index.d.ts
+++ index.d.ts
@@ -1,61 +1,83 @@
-import { Component, AppContext, App } from 'vue';
+import type { Component, AppContext, App } from "vue";
+import type { ComponentProps } from "vue-component-type-helpers";
+import type { VDialog, VCard, VCardTitle, VCardText, VCardActions, VBtn, VTextField, VSnackbar } from "vuetify/components";
+type DialogProps = InstanceType<typeof VDialog>["$props"];
+type CardProps = InstanceType<typeof VCard>["$props"];
+type CardTitleProps = InstanceType<typeof VCardTitle>["$props"];
+type CardTextProps = InstanceType<typeof VCardText>["$props"];
+type CardActionsProps = InstanceType<typeof VCardActions>["$props"];
+type BtnProps = InstanceType<typeof VBtn>["$props"];
+type TextFieldProps = InstanceType<typeof VTextField>["$props"];
+type SnackbarProps = InstanceType<typeof VSnackbar>["$props"];
 
-export interface DialogOptions {
-  /** Vue Component to mount inside the dialog */
-  component?: Component;
-  /** Props to pass to the component */
-  componentProps?: Record<string, any>;
-  /** Props to pass to the v-dialog component */
-  dialogProps?: Record<string, any>;
-  /** Theme to use */
+export interface DialogOptions<C extends Component = Component> {
+  component?: C;
+  componentProps?: ComponentProps<C>;
+  dialogProps?: DialogProps;
   theme?: string;
-  /** Optional application context */
   appContext?: AppContext;
 }
 
-export type UseDialogFn = (options?: DialogOptions) => Promise<any>;
+export type UseDialogFn = <C extends Component = Component>(options?: DialogOptions<C>) => Promise<any>;
 
-export interface ConfirmDialogOptions {
+export interface ConfirmDialogOptions<
+  TitleC extends Component = Component,
+  ContentC extends Component = Component,
+  ActionsC extends Component = Component,
+> {
   title?: string;
-  titleComponent?: Component;
-  titleComponentProps?: Record<string, any>;
+  titleComponent?: TitleC;
+  titleComponentProps?: ComponentProps<TitleC>;
+
   content?: string;
-  contentComponent?: Component;
-  contentComponentProps?: Record<string, any>;
-  actionsContentComponent?: Component;
+  contentComponent?: ContentC;
+  contentComponentProps?: ComponentProps<ContentC>;
+
+  actionsContentComponent?: ActionsC;
+
   confirmationText?: string;
   cancellationText?: string;
-  dialogProps?: Record<string, any>;
-  cardProps?: Record<string, any>;
-  cardTitleProps?: Record<string, any>;
-  cardTextProps?: Record<string, any>;
-  cardActionsProps?: Record<string, any>;
-  cancellationButtonProps?: Record<string, any>;
-  confirmationButtonProps?: Record<string, any>;
+
+  dialogProps?: DialogProps;
+  cardProps?: CardProps;
+  cardTitleProps?: CardTitleProps;
+  cardTextProps?: CardTextProps;
+  cardActionsProps?: CardActionsProps;
+  cancellationButtonProps?: BtnProps;
+  confirmationButtonProps?: BtnProps;
+
   confirmationKeyword?: string;
-  confirmationKeywordTextFieldProps?: Record<string, any>;
+  confirmationKeywordTextFieldProps?: TextFieldProps;
+
   theme?: string;
   appContext?: AppContext;
 }
 
-export type UseConfirmFn = (options?: ConfirmDialogOptions) => Promise<boolean>;
+export type UseConfirmFn = <TitleC extends Component = Component, ContentC extends Component = Component, ActionsC extends Component = Component>(
+  options?: ConfirmDialogOptions<TitleC, ContentC, ActionsC>
+) => Promise<boolean>;
 
-export interface SnackbarOptions {
+export interface SnackbarOptions<ContentC extends Component = Component> {
   text?: string;
-  contentComponent?: Component | string;
-  snackbarProps?: Record<string, any>;
+  contentComponent?: ContentC;
+  contentComponentProps?: ComponentProps<ContentC>;
+  snackbarProps?: SnackbarProps;
   showCloseButton?: boolean;
-  closeButtonProps?: Record<string, any>;
+  closeButtonProps?: BtnProps;
   closeButtonText?: string;
   theme?: string;
   appContext?: AppContext;
 }
 
-export type UseSnackbarFn = (options?: SnackbarOptions) => void;
+export type UseSnackbarFn = <ContentC extends Component = Component>(options?: SnackbarOptions<ContentC>) => void;
 
-export function useDialog(globalOptions?: DialogOptions): UseDialogFn;
-export function useConfirm(globalOptions?: ConfirmDialogOptions): UseConfirmFn;
-export function useSnackbar(globalOptions?: SnackbarOptions): UseSnackbarFn;
+export function useDialog<C extends Component = Component>(globalOptions?: DialogOptions<C>): UseDialogFn;
+
+export function useConfirm<TitleC extends Component = Component, ContentC extends Component = Component, ActionsC extends Component = Component>(
+  globalOptions?: ConfirmDialogOptions<TitleC, ContentC, ActionsC>
+): UseConfirmFn;
+
+export function useSnackbar<ContentC extends Component = Component>(globalOptions?: SnackbarOptions<ContentC>): UseSnackbarFn;
 
 export const ConfirmDialog: Component;
 
@@ -71,7 +93,7 @@ declare const plugin: {
 
 export default plugin;
 
-declare module '@vue/runtime-core' {
+declare module "@vue/runtime-core" {
   export interface ComponentCustomProperties {
     $dialog: UseDialogFn;
     $confirm: UseConfirmFn;
